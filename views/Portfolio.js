9f2de81d0016f53ed3082ed3feb83653db469eeb
Cancerlot <30314847+Cancerlot@users.noreply.github.com>
2017-12-15 13:18:19 +0800

    bettterrr again

--- views/Portfolio.js
+++ views/Portfolio.js
@@ -1,32 +1,38 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-let i = 0;
-const text = 'Hi, I am Glen and I am a Front-end Web Developer who is motivated to combine the art of design with the aptitude of programming.';
+  /*function scrollWin() {
+    window.scrollTo(0,0);
+  }
+  scrollWin();
+  */
 
-function delay(){
-  setTimeout(function intro() {
-    if (i < text.length) {
-      document.querySelector('#description').innerHTML += text.charAt(i);
-      i++;
-      setTimeout(intro, 50);
-    }
-  },1000)
-}
+  let i = 0;
+  const text = 'Daily routine: Open eyes, turn on Computer, Make coffee, open IDE, Code away.';
 
-delay();
+  function delay() {
+    setTimeout(function intro() {
+      if (i < text.length) {
+        document.querySelector('#description').innerHTML += text.charAt(i);
+        i++;
+        setTimeout(intro, 50);
+      }
+    }, 1000)
+  }
+
+  delay();
 
 
   //scroll effects
 
-  const me = document.querySelector('#meLink').addEventListener('click', scrollTo),
+  const me = document.querySelector('.navbar-brand').addEventListener('click', scrollTo),
 
-  skill = document.querySelector('#skillsLink').addEventListener('click', scrollTo1),
+    skill = document.querySelector('#skillsLink').addEventListener('click', scrollTo1),
 
-  portfolio =
-  document.querySelector('#projectLink').addEventListener('click', scrollTo2),
+    portfolio =
+    document.querySelector('#projectLink').addEventListener('click', scrollTo2),
 
-  contact =
-  document.querySelector('#contactLink').addEventListener('click', scrollTo3);
+    contact =
+    document.querySelector('#contactLink').addEventListener('click', scrollTo3);
 
   function scrollTo() {
     document.querySelector('#me').scrollIntoView({
@@ -55,5 +61,50 @@ delay();
       block: 'start'
     });
   }
+
+  $('.resizeImg').mouseover(function() {
+    $(this).css('animation', 'dissapear 0.5s 1 forwards')
+  })
+
+  $('.resizeImg').mouseout(function() {
+    $(this).css('animation', 'appear 0.5s 1 forwards')
+  })
+
+  //form validation
+
+  $('form').bootstrapValidator({
+    fields: {
+      name: {
+        validators: {
+          stringLength: {
+            min: 2,
+          },
+          notEmpty: {
+            message: 'Please enter your name'
+          }
+        }
+      },
+      email: {
+        validators: {
+          stringLength: {
+            min: 5,
+          },
+          notEmpty: {
+            message: 'Please enter your email'
+          }
+        }
+      },
+      message: {
+        validators: {
+          stringLength: {
+            min: 10,
+          },
+          notEmpty: {
+            message: 'Enter more then 10 characters!'
+          }
+        }
+      }
+    }
+  })
 
 });
